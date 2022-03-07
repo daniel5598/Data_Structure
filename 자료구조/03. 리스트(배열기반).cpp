1c34833b8ee07509790df35876b590aa9c0ed4b1
Bum Joon Suh <daniel5598@naver.com>
2022-03-07 13:54:30 +0900

    03. 리스트(배열기반)

--- 자료구조/03. 리스트(배열기반).cpp
+++ 자료구조/03. 리스트(배열기반).cpp
@@ -1,103 +1,103 @@
-//#include<stdio.h>
-//#include<string.h>
-//#define MAX_SIZE 50
-//typedef struct{
-//	char name[MAX_SIZE];
-//	int num;
-//	int price;
-//	char sort[MAX_SIZE];
-//}item;
-//void enter(item* far,int i) // *==[]
-//{
-//	printf("품명을 입력하시오:");
-//	scanf("%s",&far[i].name);
-//	printf("수량을 입력하시오:");
-//	scanf("%d",&far[i].num);
-//	printf("단가를 입력하시오:");
-//	scanf("%d",&far[i].price);
-//	printf("구분을 하시오(식품, 문구, 의류):");
-//	scanf("%s",&far[i].sort);
-//}
-//void disp(item* far, int i)
-//{
-//	printf("\n%10s%10s%10s%10s\n", "품명", "수량", "단가", "구분");
-//	for (int m = 0; m < i; m++)
-//	{
-//		printf("%10s%10d%10d%10s", far[m].name, far[m].num, far[m].price, far[m].sort);
-//		putchar('\n');
-//	}
-//}
-//void del(item* far, int i)
-//{
-//	char tmpname[MAX_SIZE];
-//	int m;
-//	printf("품명을 입력하시오:");
-//	scanf("%s",&tmpname);
-//	for (m = 0; m < i; m++)
-//	{
-//		if (strcmp(tmpname ,far[m].name)==0)break;
-//	}
-//	for (int n = m; n < i; n++)
-//	{
-//		far[n] = far[n+1];
-//	}
-//}
-//void add(item* far, int i)
-//{
-//	char tmpname[MAX_SIZE];
-//	int m,n;
-//	printf("삽입할 품명을 입력하시오:");
-//	scanf("%s",&tmpname);
-//	for (m = 0; m < i; m++)
-//	{
-//		if (strcmp(tmpname, far[m].name) == 0)break;
-//	}
-//	for (n = i; n> m; n--)
-//	{
-//		far[n] = far[n - 1];
-//	}
-//	printf("품명을 입력하시오:");
-//	scanf("%s", &far[m].name);
-//	printf("수량을 입력하시오:");
-//	scanf("%d", &far[m].num);
-//	printf("단가를 입력하시오:");
-//	scanf("%d", &far[m].price);
-//	printf("구분을 하시오(식품, 문구, 의류):");
-//	scanf("%s", &far[m].sort);
-//}
-//void main()
-//{
-//	item arr[10];
-//	int num,i=0;
-//	
-//	for (;;) {
-//		printf("1.추가\n2.출력\n3.삭제\n4.삽입\n5.종료\n->");
-//		scanf("%d",&num);
-//		if (num == 1) 
-//		{
-//			enter(arr,i++);
-//		}
-//		else if (num == 2)
-//		{
-//			disp(arr, i);
-//		}
-//		else if (num == 3)
-//		{
-//			del(arr, i);
-//			i--;
-//		}
-//		else if (num == 4)
-//		{
-//			i++;
-//			add(arr, i);
-//		}
-//		else if (num == 5)break;
-//		else
-//		{
-//			printf("다시 입력해주세요.");
-//		}
-//	}
-//}
+#include<stdio.h>
+#include<string.h>
+#define MAX_SIZE 50
+typedef struct{
+	char name[MAX_SIZE];
+	int num;
+	int price;
+	char sort[MAX_SIZE];
+}item;
+void enter(item* far,int i) // *==[]
+{
+	printf("품명을 입력하시오:");
+	scanf("%s",&far[i].name);
+	printf("수량을 입력하시오:");
+	scanf("%d",&far[i].num);
+	printf("단가를 입력하시오:");
+	scanf("%d",&far[i].price);
+	printf("구분을 하시오(식품, 문구, 의류):");
+	scanf("%s",&far[i].sort);
+}
+void disp(item* far, int i)
+{
+	printf("\n%10s%10s%10s%10s\n", "품명", "수량", "단가", "구분");
+	for (int m = 0; m < i; m++)
+	{
+		printf("%10s%10d%10d%10s", far[m].name, far[m].num, far[m].price, far[m].sort);
+		putchar('\n');
+	}
+}
+void del(item* far, int i)
+{
+	char tmpname[MAX_SIZE];
+	int m;
+	printf("품명을 입력하시오:");
+	scanf("%s",&tmpname);
+	for (m = 0; m < i; m++)
+	{
+		if (strcmp(tmpname ,far[m].name)==0)break;
+	}
+	for (int n = m; n < i; n++)
+	{
+		far[n] = far[n+1];
+	}
+}
+void add(item* far, int i)
+{
+	char tmpname[MAX_SIZE];
+	int m,n;
+	printf("삽입할 품명을 입력하시오:");
+	scanf("%s",&tmpname);
+	for (m = 0; m < i; m++)
+	{
+		if (strcmp(tmpname, far[m].name) == 0)break;
+	}
+	for (n = i; n> m; n--)
+	{
+		far[n] = far[n - 1];
+	}
+	printf("품명을 입력하시오:");
+	scanf("%s", &far[m].name);
+	printf("수량을 입력하시오:");
+	scanf("%d", &far[m].num);
+	printf("단가를 입력하시오:");
+	scanf("%d", &far[m].price);
+	printf("구분을 하시오(식품, 문구, 의류):");
+	scanf("%s", &far[m].sort);
+}
+void main()
+{
+	item arr[10];
+	int num,i=0;
+	
+	for (;;) {
+		printf("1.추가\n2.출력\n3.삭제\n4.삽입\n5.종료\n->");
+		scanf("%d",&num);
+		if (num == 1) 
+		{
+			enter(arr,i++);
+		}
+		else if (num == 2)
+		{
+			disp(arr, i);
+		}
+		else if (num == 3)
+		{
+			del(arr, i);
+			i--;
+		}
+		else if (num == 4)
+		{
+			i++;
+			add(arr, i);
+		}
+		else if (num == 5)break;
+		else
+		{
+			printf("다시 입력해주세요.");
+		}
+	}
+}
 
 #include<stdio.h>
 #include<stdlib.h>
